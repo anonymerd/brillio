a3a6c7945a2a7aeff4410284f1d7ebb26f9c824c
Rohit Bisht <Rohit.Bisht@BRILLIO.COM>
2022-09-11 22:47:03 +0530

    Added spring boot related practice stuff

---
+++ Spring_Boot_Project/src/main/java/com/rohit/sb_project/SpringbootprojectzeroApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.sb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectzeroApplication.class, args);
+	}
+
+}
